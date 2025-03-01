84381c931c339e7c88391a415e524af5b8950f3e
sanjid <sanjidtalukder02@gmail.com>
2025-03-01 11:07:47 +0600

    CloudSync of completed button and work propery

--- style.js
+++ style.js
@@ -25,12 +25,49 @@
         let secounds = now.getSeconds().toString().padStart(2,'0');
         let amPm = hours%2 ? "PM":"AM";
         hours = hours%12 || 12;
+      
         const formattedTime = `${hours}:${minutes}:${secounds}`;
-        messages.textContent =`You have completed the task 
-        fix Mobile Button issue at ${formattedTime} ${amPm}`;
+        const titleSet= document.title = `Fix Mobile Button`;
+        messages.innerHTML =`You have completed the task 
+        <strong>${titleSet}</strong> issue at ${formattedTime} ${amPm}`;
 
     }
 });
 
+// CloudSync button click here
 
+const deadlines =document.getElementById("cloudSync").addEventListener("click",function(){
+    alert("Board update Successfully");
+    
+    const pending = document.getElementById("taskAssi");
+    const tracjMark = document.getElementById("track-mark");
+    const button= document.getElementById("cloudSync");
+    const messages =document.getElementById("message");
+    
+    const pendingCount = parseInt(pending.textContent);
+    const tracjMarkCount = parseInt(tracjMark.textContent);
+    
+    if (pendingCount > 0) {
+        pending.textContent = pendingCount - 1;
+        tracjMark.textContent = tracjMarkCount + 1;
+    
+        button.disabled =true;
+        button.style.backgroundColor="gray";
+        button.style.cursore ="not-allowed";
+    
+    
+        let now1 =new Date();
+        let hours1 = now1.getHours().toString().padStart(2,'0');
+       let minutes1 = now1.getMinutes().toString().padStart(2,'0');
+        let secounds1 = now1.getSeconds().toString().padStart(2,'0');
+        let amPm1 = hours1%2 ? "PM":"AM";
+        hours1 = hours1%12 || 12;
+        const formattedTime1 = `${hours1}:${minutes1}:${secounds1} ${amPm1}`;
+       const titleSet= document.title = `Fix Mobile Button`
+        messages.innerHTML +=`<p>You have completed the task 
+       <strong>${titleSet}</strong> issue at ${formattedTime1}  </p>`;
+    
+    
+    }
+    })
 
