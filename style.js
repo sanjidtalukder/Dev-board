d10eecb5281267cfcdd49162bf8ac6eebc68dd16
sanjid <sanjidtalukder02@gmail.com>
2025-03-01 12:06:30 +0600

    Thsi is GlassDore cleared button here

--- style.js
+++ style.js
@@ -146,4 +146,77 @@ const deadlines =document.getElementById("cloudSync").addEventListener("click",f
             }
             })
 
-            
+            // Google-LLC click here
+            const googlellc =document.getElementById("google-LLC").addEventListener("click",function(){
+                alert("Board update Successfully");
+                
+                const pending = document.getElementById("taskAssi");
+                const tracjMark = document.getElementById("track-mark");
+                const button= document.getElementById("google-LLC");
+                const messages =document.getElementById("message");
+                
+                const pendingCount = parseInt(pending.textContent);
+                const tracjMarkCount = parseInt(tracjMark.textContent);
+                
+                if (pendingCount > 0) {
+                    pending.textContent = pendingCount - 1;
+                    tracjMark.textContent = tracjMarkCount + 1;
+                
+                    button.disabled =true;
+                    button.style.backgroundColor="gray";
+                    button.style.cursore ="not-allowed";
+                
+                
+                    let now1 =new Date();
+                    let hours1 = now1.getHours().toString().padStart(2,'0');
+                   let minutes1 = now1.getMinutes().toString().padStart(2,'0');
+                    let secounds1 = now1.getSeconds().toString().padStart(2,'0');
+                    let amPm1 = hours1%2 ? "PM":"AM";
+                    hours1 = hours1%12 || 12;
+                    const formattedTime1 = `${hours1}:${minutes1}:${secounds1} ${amPm1}`;
+                   const titleSet= document.title = `Fix Mobile Button`
+                    messages.innerHTML +=`<p>You have completed the task 
+                   <strong>${titleSet}</strong> issue at ${formattedTime1}  </p>`;
+                
+                
+                }
+                })
+
+
+                //Glassdoar click here
+
+                const glassdoar =document.getElementById("glass-doar").addEventListener("click",function(){
+                    alert("Board update Successfully");
+                    
+                    const pending = document.getElementById("taskAssi");
+                    const tracjMark = document.getElementById("track-mark");
+                    const button= document.getElementById("glass-doar");
+                    const messages =document.getElementById("message");
+                    
+                    const pendingCount = parseInt(pending.textContent);
+                    const tracjMarkCount = parseInt(tracjMark.textContent);
+                    
+                    if (pendingCount > 0) {
+                        pending.textContent = pendingCount - 1;
+                        tracjMark.textContent = tracjMarkCount + 1;
+                    
+                        button.disabled =true;
+                        button.style.backgroundColor="gray";
+                        button.style.cursore ="not-allowed";
+                    
+                    
+                        let now1 =new Date();
+                        let hours1 = now1.getHours().toString().padStart(2,'0');
+                       let minutes1 = now1.getMinutes().toString().padStart(2,'0');
+                        let secounds1 = now1.getSeconds().toString().padStart(2,'0');
+                        let amPm1 = hours1%2 ? "PM":"AM";
+                        hours1 = hours1%12 || 12;
+                        const formattedTime1 = `${hours1}:${minutes1}:${secounds1} ${amPm1}`;
+                       const titleSet= document.title = `Fix Mobile Button`
+                        messages.innerHTML +=`<p>You have completed the task 
+                       <strong>${titleSet}</strong> issue at ${formattedTime1}  </p>`;
+                    
+                    
+                    }
+                    })
+
